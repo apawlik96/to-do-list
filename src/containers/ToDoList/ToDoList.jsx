2d7cc0f71d0e973b1c5dcc00a96e7743da06c526
Ania <aniaa.pawlik@gmail.com>
2024-11-05 12:24:36 +0100

    Improve theme functionality: implement cookie handling + remove confirmation

--- src/containers/ToDoList/ToDoList.jsx
+++ src/containers/ToDoList/ToDoList.jsx
@@ -86,6 +86,14 @@ export const ToDoList = () => {
     const cookies = cookie.parse(document.cookie || "");
     const storedTasks = cookies.tasks;
     const parsedTasks = storedTasks ? JSON.parse(storedTasks) : [];
+
+    const storedTheme = cookies.theme;
+    const isDark = storedTheme === "dark";
+    setIsDarkTheme(isDark);
+
+    document.body.classList.toggle("dark-theme", isDark);
+    document.body.classList.toggle("light-theme", !isDark);
+
     setTasks(parsedTasks);
   }, []);
 
@@ -159,10 +167,14 @@ export const ToDoList = () => {
     : tasks;
 
   const handleThemeChange = () => {
-    setIsDarkTheme((prevState) => !prevState);
-    document.body.classList.toggle("light-theme", isDarkTheme);
-    toast.info("Theme changed!", {
-      className: "custom-toast",
+    setIsDarkTheme((prevState) => {
+      const isDarkTheme = !prevState;
+      document.cookie = cookie.serialize(
+        "theme",
+        isDarkTheme ? "dark" : "light"
+      );
+      document.body.classList.toggle("light-theme", !isDarkTheme);
+      return isDarkTheme;
     });
   };
 
